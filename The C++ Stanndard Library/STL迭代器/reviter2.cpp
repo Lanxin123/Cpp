9733e458d2120ca5f6f2f9ac3b8163c59e0df1bc
Lanxin123 <337544818@qq.com>
2017-11-11 23:27:36 +0800

    The 27 days

---
+++ The C++ Stanndard Library/STL迭代器/reviter2.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include<vector>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//create list with elements from 1 to 9
+	vector<int> coll = { 1,2,3,4,5,6,7,8,9 };
+
+	//find position of element  with value 5
+	vector<int>::const_iterator pos;
+	pos = find(coll.cbegin(), coll.cend(),
+		5);
+
+	//print value to which iterator pos refers
+	cout << "pos:	" << *pos << endl;
+
+	//convert iteator to reverse iteator rpos
+	vector<int>::const_reverse_iterator rpos(pos);
+
+	//print value to which reverse iterator rpos refers
+	cout << "rpos:	" << *rpos << endl;
+}
+
+
