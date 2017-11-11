c1af0c69dafa34f3f7f50421d02113e16b09bb8a
Lanxin123 <337544818@qq.com>
2017-11-11 21:12:09 +0800

    The 27 days

---
+++ The C++ Stanndard Library/STL迭代器/iterswap.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include<list>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include "print.hpp"
+using namespace std;
+int main()
+{
+	list<int>  coll;
+
+	//insert elements from 1 to 9
+	for (int i=1;i<=9;++i)
+	{
+		coll.push_back(i);
+	}
+
+	PRINT_ELEMENTS(coll);
+
+	//swap first and second value
+	iter_swap(coll.begin(), next(coll.begin()));
+
+	PRINT_ELEMENTS(coll);
+
+	//swap first and last value
+	iter_swap(coll.begin(), next(coll.begin()));
+
+	PRINT_ELEMENTS(coll);
+
+	//swap first and last value
+	iter_swap(coll.begin(), prev(coll.end()));
+
+	PRINT_ELEMENTS(coll);
+}
