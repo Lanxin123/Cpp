13c2d01b063a7134c89ec1ac03bd99484d54b382
Lanxin123 <337544818@qq.com>
2017-11-13 18:42:07 +0800

    The 29 days

---
+++ The C++ Stanndard Library/STL函数对象及Lambda/Sequence1.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include "print.hpp"
+using namespace std;
+
+class IntSequence {
+private:
+	int value;
+public:
+	IntSequence (int initialValue)
+		: value(initialValue){}
+
+	int operator() () {				//function call
+		return ++value;		
+	}
+};
+
+int main()
+{
+	list<int> coll;
+
+	IntSequence seq(1);
+
+	//insert values from 1 to 9
+	generate_n(back_inserter(coll),	//start
+		9,													//number of elements
+		seq);						//generates values,starting with 1
+	
+	PRINT_ELEMENTS(coll);
+
+	//replace second to last element but one with values starting at 42
+	generate(next(coll.begin()),			//start
+		prev(coll.end()),							//end
+		IntSequence(42));						//generates values ,starting with 42
+
+	PRINT_ELEMENTS(coll);
+}
+
+
+
+
