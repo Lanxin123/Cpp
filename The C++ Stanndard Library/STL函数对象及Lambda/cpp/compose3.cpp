ba04cc934e811753d55aecac53c676ebc375f286
Lanxin123 <337544818@qq.com>
2017-11-14 13:26:20 +0800

    The 30 days

---
+++ The C++ Stanndard Library/STL函数对象及Lambda/cpp/compose3.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include <algorithm>
+#include <functional>
+#include <locale>
+#include <string>
+#include <iostream>
+using namespace std;
+using namespace std::placeholders;
+
+char myToupper(char c)
+{
+	std::locale loc;
+	return std::use_facet<std::ctype<char>>(loc).toupper(c);
+}
+
+int main()
+{
+	string s("Internationalization");
+	string sub("Nation");
+
+	//search substring case insensitive
+	string::iterator pos;
+	pos = search(s.begin(), s.end(),
+		sub.begin(), sub.end(),
+		bind(equal_to<char>(),
+			bind(myToupper, _1),
+			bind(myToupper, _2)));
+	if (pos != s.end())
+	{
+		cout << "\"" << sub << "\" is part of \"" << s << "\"" << endl;
+	}
+}
