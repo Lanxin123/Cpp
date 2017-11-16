f63a6a70f1e1748cfc4fc73550ab6dcc0c6ac882
Lanxin123 <337544818@qq.com>
2017-11-16 19:53:54 +0800

    The 32 days

---
+++ The C++ Stanndard Library/STL算法/foreach1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+#include "algostuff.hpp"
+using namespace std;
+
+int main()
+{
+	vector<int> coll;
+
+	INSERT_ELEMENTS(coll, 1, 9);
+
+	//call print() for each element
+	for_each(coll.cbegin(),coll.cend(),
+		[=](int elem) {
+		cout << elem << ' ';
+	});
+	cout << endl;
+}
+
+
+
+
---
+++ The C++ Stanndard Library/STL算法/minmax1.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include "algostuff.hpp"
+#include <cstdlib>
+using namespace std;
+
+bool absLess(int elem1, int elem2)
+{
+	return abs(elem1) < abs(elem2);
+}
+int main()
+{
+	deque<int> coll;
+
+	INSERT_ELEMENTS(coll, 2, 6);
+	INSERT_ELEMENTS(coll, -3, 6);
+
+	PRINT_ELEMENTS(coll);
+
+	//process and print minimum and maximum
+	cout << "minimum:	"
+		<< *min_element(coll.cbegin(), coll.cend())
+		<< endl;
+	cout << "maximum:	"
+		<< *max_element(coll.cbegin(), coll.cend())
+		<< endl;
+	
+	//print min and max and their distance using minmax_element()
+	auto mm = minmax_element(coll.cbegin(), coll.cend());
+	cout << "min:	" << *(mm.first) << endl;
+	cout << "max:	" << *(mm.second) << endl;
+	cout << "distance:	" << distance(mm.first, mm.second) << endl;
+
+	//process and print minimum and maximum of absolute values
+	cout << "minimum of absolute values:	"
+		<< *min_element(coll.cbegin(), coll.cend(),
+			absLess)
+		<< endl;
+	cout << "maximum of absolute values:	"
+		<< *max_element(coll.cbegin(), coll.cend(),
+			absLess)
+		<< endl;
+}
+
+
